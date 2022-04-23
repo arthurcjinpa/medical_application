529e95318831b648e7eb7ef819018f97684d7887
arthurcjinpa <73398493+arthurcjinpa@users.noreply.github.com>
2022-04-23 12:30:19 +0200

    First commit :)

---
+++ src/test/java/medical_analytical_prescription/demo/MedicalAnalyticalPrescriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package medical_analytical_prescription.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAnalyticalPrescriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
